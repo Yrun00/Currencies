610217dcb97634d9a6216f583b30773befb66aa9
Yrun00 <155985757+Yrun00@users.noreply.github.com>
2024-02-25 21:23:21 +0400

    Multi Modules + dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,25 +1,18 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
-
-}
-
-kotlin {
-    jvmToolchain(17)
+    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.yrun.currencies"
+    namespace = "com.yrun.data"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.yrun.currencies"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -41,11 +34,23 @@ android {
 }
 
 dependencies {
-    implementation(project(":data"))
     implementation(project(":domain"))
-    implementation(project(":presentation"))
 
 
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.room:room-ktx:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
 }
